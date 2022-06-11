59b76e4cc08b030bae29b6b12807073db2c21c9c
Sanjithj <sanjithj591997@gmail.com>
2022-06-11 17:40:04 +0530

    Added Hash Map and Hash Table

---
+++ HashTable.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+import java.awt.List;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class HashTable {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> input = Arrays.asList(1,4,5,7,3,8,78,34.24,12,14);
+		
+		int initialCapacity = 10;
+		Object buckets[] = new Object[intialCapacity];
+		
+			bucket[i] = new LinkedList<Integer>();
+		}
+		for(int i=0;i<=input.size();i++) {
+			int index = input.get(i) % intialCapacity
+			Linkedlist
+			list.add
+		}
+		for(int i=0;i<=input.size();i++) {
+			int index = input.get(i) % intialCapacity
+			Linkedlist
+			list
+		System.out.println(input);
+	}
+}
